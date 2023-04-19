f18cadb787e2966321ce07cc16b9750565142da9
Michael Gumbley <michaelgumbley@gmail.com>
2023-04-19 23:48:37 +1000

    Login page changes - now a functioning captcha solution

--- js/auth-script.js
+++ js/auth-script.js
@@ -1,9 +1,15 @@
 
+
 //module-level vars
 var credentialsUrl = "resources/credentials.json";
 var credentialsArray;
+var canvasElement;
+var answer = 0;
+var isHuman = false;
+const maxTries = 2;
+var countTries = 0;
 
-//add submit event listener
+//add submit event listener - called after credentials loaded
 function addSubmitListener(){
 
 	document.querySelector("#submit").addEventListener("click", function (event) { 
@@ -18,6 +24,13 @@ function addSubmitListener(){
 
 	  	return val.email === user.email && val.password === user.password;
 		});
+
+        //check for valid security check
+        if(! isHuman){
+            document.querySelector("#securityOutcome").style = "color:red;"
+            document.querySelector("#securityOutcome").innerHTML = "Security check required."
+            return; //exit
+        };
 
 		if (index !== -1) {
 			//TO DO: create authentication token here for security management
@@ -52,16 +65,108 @@ function getLoginCredentials(){
 };
 
 
+function resetCanvas(){
+    console.log("Resetting challenge");
+
+    let num1 = Math.round(Math.random() * 6) + 3;
+    let num2 = Math.round(Math.random() * 5) + 1;
+    answer = num1 + num2;
+
+    let challengeStr = num1 + " + " + num2;
+    console.log(challengeStr, answer);
+
+    var ctx = canvasElement.getContext("2d");
+    ctx.scale(1, 1);
+    ctx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    ctx.font = "18px Arial italics";
+    ctx.strokeText(challengeStr, 20, 20);
+};
+
+function checkChallenge(){
+
+    //do nothing if check has already passed
+    if(isHuman) return;
+
+    //increment tries
+    countTries++;
+
+    //get input and check
+    let userInput = document.querySelector("#securityInput").value;
+    userInput = Math.trunc(Number(userInput));
+
+    if(userInput == answer){
+        isHuman = true;
+
+        //show success msg
+        document.querySelector("#securityOutcome").style = "color:green;"
+        document.querySelector("#securityOutcome").innerHTML = "Check succeeded!"
+
+        //disable input box and buttons
+        document.querySelector("#checkBtn").style.color = "gray"
+        document.querySelector("#resetBtn").style = "color:gray;"
+        document.querySelector("#securityInput").disabled = true;
+    }
+    else{
+        //fail condition
+        isHuman = false;
+
+        //check tries
+        if(countTries > maxTries){
+            alert("Security check failed. Resetting challenge.");
+            resetChallenge();
+        }
+        else{
+            //show fail msg
+            document.querySelector("#securityOutcome").style = "color:red;"
+            document.querySelector("#securityOutcome").innerHTML = "Input is incorrect. Try again."
+            document.querySelector("#securityInput").focus();
+        }
+    }
+};
+
+function resetChallenge(){
+
+    //do nothing if check has already passed
+    if(isHuman) return;
+
+    //ensure "check" button has text
+    countTries = 0;
+    // let checkBtn = document.querySelector("#checkBtn");
+    // checkBtn.value = "Check";
+    // checkBtn.style = 'font-family: "Helvetica Neue",Helvetica,Arial,sans-serif;';
+
+    //clear input box
+    document.querySelector("#securityInput").value = "";
+
+    //reset canvas
+    resetCanvas();
+};
+
+//UX candy to clear error message on input
+document.querySelector("#securityInput").addEventListener("keydown", function (event) { 
+
+    //if an attempt has been made, there should be a meesage.  Remove it.
+    if(countTries > 0){
+    document.querySelector("#securityOutcome").innerHTML = " ";
+    }
+
+});
+
+
 
 // On page load - before images or CSS 
 $(function () { // Same as document.addEventListener("DOMContentLoaded"...
 
-  //async call to get the auth object
+    //async call to get the auth object
 	credentialsArray = getLoginCredentials();
     
 
-  //then enable the submit button
-	addSubmitListener();
+    //then enable the submit button
+    addSubmitListener();
+
+    //set canvas
+    canvasElement = document.querySelector("#mathCanvas");
+    resetCanvas();
 
     
   });  
